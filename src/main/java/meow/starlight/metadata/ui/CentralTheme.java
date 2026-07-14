839deb2530fc70abddc36340d2df8814b1c70f64
StarlightAura <auracv.vs@gmail.com>
2026-07-14 23:49:58 +0200

    start of materialfx theming

---
+++ src/main/java/meow/starlight/metadata/ui/CentralTheme.java
@@ -0,0 +1,17 @@
+package meow.starlight.metadata.ui;
+
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.*;
+
+public class CentralTheme {
+    public void start(){
+        UserAgentBuilder.builder()
+                .themes(JavaFXThemes.MODENA)
+                .themes(MaterialFXStylesheets.forAssemble(true))
+                .setDeploy(true)
+                .setResolveAssets(true)
+                .build()
+                .setGlobal();
+    }
+}
